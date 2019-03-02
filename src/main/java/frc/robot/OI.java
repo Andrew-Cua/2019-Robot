024f94abca2ab64b313f14239e6dd82f0dea7580
Andrew-Cua <andrew@cuafamily.com>
2019-03-02 16:50:00 -0500

    minor updates

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import frc.robot.commands.*;
-import frc.robot.commands.ArmStateCommands.*;
+import frc.robot.commands.ArmCommands.*;
 import frc.robot.subsystems.Arm_Subsys.ArmSetpoints;
 
 public class OI {
--- src/main/java/frc/robot/commands/ArmCommands/DefaultArmCommand.java
+++ src/main/java/frc/robot/commands/ArmCommands/DefaultArmCommand.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands.ArmStateCommands;
+package frc.robot.commands.ArmCommands;
 
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Command;
